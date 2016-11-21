522ae528e9382534711e08cc0dc0a98f9af5332c
Manuel Palomo Ramon <palomoraising@gmail.com>
2016-11-21 21:47:10 +0100

    Laser Defender created

---
+++ Laser Defen/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+	public GameObject enemyPrefab;
+
+	void Start() {
+		GameObject enemy = (GameObject)Instantiate(enemyPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+		enemy.transform.parent = this.transform;
+
+	}
+
+	void Update() {
+	
+	}
+}
---
+++ Laser Defen/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public int speed;
+
+	private float maximumXPosition;
+	private float minimumXPosition;
+
+	void Start() {
+		float spritePadding = 1f;
+		float distanceBetweenCameraAndPlayer = transform.position.z - Camera.main.transform.position.z;
+
+		Vector3 leftMostPosition = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distanceBetweenCameraAndPlayer));
+		Vector3 rightMostPosition = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distanceBetweenCameraAndPlayer));
+
+		this.minimumXPosition = leftMostPosition.x + spritePadding;
+		this.maximumXPosition = rightMostPosition.x - spritePadding;
+	}
+
+
+	void Update() {
+		HandleMovement();	
+	}
+
+	private void HandleMovement() {
+		if(Input.GetKey(KeyCode.RightArrow)) {
+			MoveRight();
+
+		} else if(Input.GetKey(KeyCode.LeftArrow)) {
+			MoveLeft();
+		}
+		RestrictMovement();
+	}
+
+	private void MoveRight() {
+		this.transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+	}
+
+	private void MoveLeft() {
+		this.transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+	}
+
+	private void RestrictMovement() {
+		float newX = Mathf.Clamp(this.transform.position.x, minimumXPosition, maximumXPosition);
+		this.transform.position = new Vector2(newX, this.transform.position.y);
+	}
+
+
+}
