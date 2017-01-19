c41f4a2afca6dcff13c033e252256bfbe7111bad
Manuel Palomo Ramón <Manuel Palomo Ramn>
2017-01-19 18:01:36 +0100

    Iniciado glitch garden

---
+++ Glitch Garden/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+
+	static MusicPlayer instance = null;
+
+	public AudioClip startClip;
+	public AudioClip gameClip;
+
+	private AudioSource music;
+
+	void Awake() {
+		if(instance != null && instance != this) {
+			Destroy(gameObject);
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+			music = GetComponent<AudioSource>();
+		}
+
+	}
+
+	void OnEnable() {
+		SceneManager.sceneLoaded += OnLevelFinishedLoading;
+	}
+
+	void OnDisable() {
+		SceneManager.sceneLoaded -= OnLevelFinishedLoading;
+	}
+
+	void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode) {
+		music.Stop();
+		if(scene.name == "Start") {
+			music.clip = startClip;
+		} else {
+			music.clip = gameClip;
+		}
+		music.Play();
+		
+	}
+}
