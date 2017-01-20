89ac8e33c57b33ab91f1a1f16b94ce4b08b5d839
Manuel Palomo Ramón <Manuel Palomo Ramn>
2017-01-20 21:27:26 +0100

    Nearly finished menus

---
+++ Glitch Garden/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioClip[] musicInLevel;
+
+	private AudioSource audioSource;
+
+	void OnEnable() {
+		SceneManager.sceneLoaded += OnSceneLoaded;
+	}
+
+	void Awake() {
+		DontDestroyOnLoad(gameObject);
+	}
+
+	void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
+		audioSource = GetComponent<AudioSource>();
+		int level = scene.buildIndex;
+		if(musicInLevel[level] != null) {
+			audioSource.clip = musicInLevel[level];
+			audioSource.loop = true;
+			audioSource.Play();
+		}
+	}
+}
